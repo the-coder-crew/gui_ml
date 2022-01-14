0178a0291ed7d50712c2b88488e107e9817860c9
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2022-01-14 20:49:20 +0530

    Refactored style in home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,62 +1,75 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
 
 import mlBg from '../img/ml_bg.jpg'
 import playButton from '../img/play_button.svg'
+
+const Div = styled.div`
+    background: linear-gradient( rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5) ), url(${mlBg});
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+`
+
+const ImgButton = styled.button`
+    background: none;
+    border: none;
+    border-radius: '50%';
+    width: 13%;
+    margin: 0 auto;
+
+    &:hover .img-button {
+        width: 15%;
+      }
+`
+
+const divStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+}
+
+const H1 = styled.h1`
+    padding: 0;
+    margin: 0;
+    margin-top: 0.5em;
+    color: #ffffffad;
+    font-size: 2.5em;
+`
+
+const P = styled.p`
+    color: #ffffffad;
+    font-size: 1.5em;
+    width: 60%;
+    margin: 1.5em auto;
+`
 
 const Home = () => {
     const navigate = useNavigate()
 
     return (
-        <div style={{
-            background:  `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.1) ), url(${mlBg})`,
-            backgroundSize: '125% 110%',
-            backgroundRepeat: 'no-repeat',
-            margin: 0,
-            padding: 0,
-            width: '100%',
-            height: '100vh',
-            display: 'flex',
-            alignContent: 'center',
-            justifyContent: 'center',
-        }}>
-            <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignContent: 'center',
-                    justifyContent: 'center',
-                    textAlign: 'center',
-                }}>
-                <button
-                    onClick={() => navigate('/content')}
-                    style={{
-                        background: 'none',
-                        border: 'none',
-                        borderRadius: '50%',
-                    }}>
-                    <img src={playButton} alt='' 
-                        style={{
-                            width: '13%',
-                        }}/>
-                </button>
+        <Div>
+            <div style={ divStyle }>
+                <ImgButton onClick={() => navigate('/content')} className='img-button'>
+                    <img src={playButton} 
+                        alt=''
+                        style={{ width: '100%', }}/>
+                </ImgButton>
 
-                <h1 style={{
-                    padding: 0,
-                    margin: 0,
-                    marginTop: '0.5em',
-                    color: '#ffffffad',
-                    fontSize: '2.5em',
-                }}>No Code Machine Learning</h1>
+                <H1>No Code Machine Learning</H1>
 
-                <p style={{
-                    color: '#ffffffad',
-                    fontSize: '1.5em',
-                    width: '60%',
-                    margin: '1.5em auto',
-                }}>Machine learning is about machine learning algorithms. You need to know what algorithms are available for a given problem, how they work, and how to get the most out of them.</p>
+                <P>Machine learning is about machine learning algorithms. You need to know what algorithms are available for a given problem, how they work, and how to get the most out of them.</P>
             </div>
-        </div>
+        </Div>
     )
 }
 
