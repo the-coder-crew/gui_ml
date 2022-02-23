4010059f152cc9cecd0de808b0e17e35b6605bf7
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2022-02-23 10:55:08 +0530

    Added the basic backend

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+// const cors = require('cors')
+require('express-async-errors')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+// app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.get('/', (request, response) => {
+    response.send('<p>Gui ML backend api</p>')
+})
+
+app.use(middleware.unknownEndPoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
